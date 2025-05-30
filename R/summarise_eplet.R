49132c479861e0fa1136f9c0a19a78ea5512a6a9
theHumanBorch <ncborch@gmail.com>
2025-05-30 10:11:27 -0500

    Update summarise_eplet.R
    update name pulls

--- R/summarise_eplet.R
+++ R/summarise_eplet.R
@@ -3,12 +3,13 @@ summarise_eplet <- function(df) {
   # Generate a summary of eplets by group
   dff <- df %>%
     transmute(
-      Epitope = `Epitope Name`,
+      Epitope = Epitope.Name,
       SAB = ifelse(SAB_unique != 0, 1, 0),
       PRA = ifelse(PRA_unique != 0, 1, 0),
       Mix = ifelse(Mix_unique != 0, 1, 0),
       Explex = ifelse(EXPLEX_unique != 0, 1, 0)
-    )
+    ) %>%
+    unique()
   
   # Create the intersection combination string
   dff$intersections <- apply(dff[, c("SAB", "PRA", "Mix", "Explex")], 1, function(row) {
@@ -19,6 +20,23 @@ summarise_eplet <- function(df) {
       return(paste(sort(cols_with_1), collapse = "_"))
     }
   })
+  
+  # Some duplicated epitopes, assigning priority
+  category_priority <- c(
+    "Explex_Mix_PRA_SAB" = 1,
+    "Mix_PRA_SAB" = 2,
+    "Explex_Mix_SAB" = 3,
+    "PRA_SAB" = 4,
+    "Mix_PRA" = 5,
+    "SAB" = 6
+  )
+  
+  dff <- dff %>%
+    mutate(priority = category_priority[intersections]) %>%
+    group_by(Epitope) %>%
+    slice_min(order_by = priority, n = 1) %>%
+    ungroup() %>%
+    select(-priority)
   
   # Summarize
   final_results_df <- dff %>%
