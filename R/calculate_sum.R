743c6ad34ff8dbd2ee616ea5d419ac9eeecac3a0
theHumanBorch <ncborch@gmail.com>
2025-05-30 10:11:01 -0500

    Update calculate_sum.R
    new function for new eplet registry pull

--- R/calculate_sum.R
+++ R/calculate_sum.R
@@ -1,42 +1,43 @@
-#' @title Calculate Sum of Eplet Representation
-#' @description Adds columns to the data frame based on allele matches.
-#' @param df Eplet data frame with alleles.
-#' @param alleles Vector of alleles to match.
-#' @param sum_column_name Name of the new column to store the eplet sum.
-#' @param return.sum Return the eplet data frame with a new column with the sum (TRUE)
-#' or a data frame with a set of new columns binarized by allele.
-#' @param return.dose Count unique values (FALSE) or total values/dose of the assay (TRUE)
-#' @return Updated data frame with new columns.
 calculate_sum <- function(df, 
                           alleles, 
                           sum_column_name, 
                           return.sum = TRUE, 
                           return.dose = FALSE) {
+  #Ensure no * in alleles - will ruin regex
+  df$Alleles <- gsub("[*]", "_", df$Alleles)
   
+  # Remove alleles with N suffix
   alleles <- alleles[!grepl("N$", alleles)]
-  if(return.sum) {
-  df[[sum_column_name]] <- sapply(df$Alleles, function(allele_string) {
-      # Split the allele string by commas
-      alleles_in_row <- unlist(strsplit(allele_string, ","))
-      # Count the matches with `grep` for the unique alleles
-      if(return.dose) {
-        #Discussion was to remove DRB3/4/5 duplicated alleles from dosage
+  
+  # Escape the input allele list for regex safety
+  escaped_alleles <- stringr::str_replace_all(alleles, "([\\^$.|?*+(){}\\[\\]])", "\\\\\\1")
+  
+  if (return.sum) {
+    df[[sum_column_name]] <- sapply(df$Alleles, function(allele_string) {
+      # Parse and escape the alleles in this row
+      alleles_in_row <- unlist(strsplit(allele_string, ",\\s*"))
+      escaped_row_alleles <- stringr::str_replace_all(alleles_in_row, "([\\^$.|?*+(){}\\[\\]])", "\\\\\\1")
+      
+      if (return.dose) {
+        # Deduplicate DRB3/4/5 only once
         drb345 <- grepl("^DRB[345]", alleles)
-        filtered_alleles <- c(unique(alleles[drb345]), alleles[!drb345])
-        length(grep(paste0(alleles_in_row, collapse = "|"), filtered_alleles))
+        filtered <- c(unique(alleles[drb345]), alleles[!drb345])
+        return(sum(alleles_in_row %in% filtered))
       } else {
-        length(grep(paste0("^(", paste0(alleles_in_row, collapse = "|"), ")$"), alleles))
+        # Match escaped row alleles to escaped input alleles
+        pattern <- paste0("(", paste(escaped_row_alleles, collapse = "|"), ")")
+        return(sum(stringr::str_detect(escaped_alleles, pattern)))
       }
     })
     return(df)
+    
   } else {
     tmp <- df
     for (allele in alleles) {
-      match_column <- grepl(paste0("\\b", allele, "\\b"), df$Alleles)
-      if (any(match_column)) {
-        tmp[[allele]] <- as.integer(match_column)
-      }
+      escaped <- stringr::str_replace_all(allele, "([\\^$.|?*+(){}\\[\\]])", "\\\\\\1")
+      pattern <- paste0("\\b", escaped, "\\b")
+      tmp[[allele]] <- as.integer(stringr::str_detect(df$Alleles, pattern))
     }
-  return(tmp)
-  } 
+    return(tmp)
+  }
 }
